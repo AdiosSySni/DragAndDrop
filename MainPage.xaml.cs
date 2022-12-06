1f7aff52cc804ca43775e9c5243885f53412db73
AdiosSyS <adiossysni@gmail.com>
2022-12-06 18:36:28 +0200

    Добавьте файлы проекта.

---
+++ MainPage.xaml.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.Maui.Controls;
+
+namespace MauiApp5;
+
+public partial class MainPage : ContentPage
+{ 
+	Dictionary<string, VerticalStackLayout> _data = new Dictionary<string, VerticalStackLayout>();
+	public MainPage()
+	{
+		InitializeComponent();
+		_data.Add("firstVSL", FirstVSL);
+		_data.Add("secondVSL", SecondVSL);
+		_data.Add("thirdVSL", ThirdVSL);
+
+	}
+
+	private void DragStarting(object sender, DragStartingEventArgs e)
+	{
+		var frame = (sender as DragGestureRecognizer).Parent as Frame;
+		e.Data.Properties.Add("frame", frame);
+		
+      	
+		//foreach(var item in _data)
+		//{
+		//	item.Value.Children.Remove(frame);
+		//}
+
+        //frame.Parent.Children
+        //.Children.Remove(ChildDrag);
+    }
+
+    private void DropGestureRecognizer_Drop(object sender, DropEventArgs e)
+    {
+        
+        Frame data = e.Data.Properties["frame"] as Frame;
+        (data.Parent as VerticalStackLayout).Children.Remove(data);
+        ((sender as DropGestureRecognizer)?.Parent as VerticalStackLayout).Children.Add(data);
+	}
+
+}
+
